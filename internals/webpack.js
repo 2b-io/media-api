4e54943d8f5bce0e131fe336810fe27e0f5cd7f5
LongLH <d@dapps.me>
2018-11-22 15:36:11 +0700

    fix webpack warning message

--- internals/webpack.js
+++ internals/webpack.js
@@ -1,5 +1,5 @@
 const slsw = require('serverless-webpack')
-const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -7,7 +7,8 @@ module.exports = {
   devtool: 'source-map',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   externals: [
-    /aws-sdk/
+    /aws-sdk/,
+    nodeExternals()
   ],
   stats: 'minimal',
   module: {
@@ -41,11 +42,5 @@ module.exports = {
       'node_modules',
       'src'
     ]
-  },
-  plugins: [
-    // https://github.com/webpack/webpack/issues/3404#issuecomment-264342120
-    new webpack.DefinePlugin({
-      'typeof window': '"object"'
-    })
-  ]
+  }
 }
