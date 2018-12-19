0f2ac9b5f33d53478f78dfb91c6723e64fc7d4f2
cuongtv <truongcuong90@gmail.com>
2018-12-19 19:08:11 +0700

    - Api get metric

--- src/functions/project/metric/datapoint/list.js
+++ src/functions/project/metric/datapoint/list.js
@@ -1,11 +1,33 @@
-import { NOT_IMPLEMENTED } from 'http-status-codes'
+import { NOT_FOUND, OK } from 'http-status-codes'
+import joi from 'joi'
 
 import resource from 'rest/resource'
+import metricService from 'services/metric'
 
-export default resource(
+const SCHEMA = joi.object().keys({
+  startTime: joi.number().required(),
+  endTime: joi.number().required(),
+  period: joi.number().required()
+})
+
+export default resource('METRIC')(
   async (req) => {
+    const { projectIdentifier, metricName } = req.pathParameters
+    const params = req.queryStringParameters || {}
+
+    const values = await joi.validate(params, SCHEMA)
+
+    const listMetricData = await metricService.get(projectIdentifier, metricName, values)
+
+    if (!listMetricData) {
+      throw {
+        statusCode: NOT_FOUND
+      }
+    }
+
     return {
-      statusCode: NOT_IMPLEMENTED
+      statusCode: OK,
+      resource: listMetricData
     }
   }
 )
--- src/services/metric.js
+++ src/services/metric.js
@@ -1,11 +1,43 @@
+import ms from 'ms'
+
 import elasticsearchService from 'services/elasticsearch'
 import config from 'infrastructure/config'
 import mapping from 'mapping/metric'
 
 const DATAPOINT_VERSION = config.elasticsearch.datapointVersion
 
-const update = async (projectIdentifier, metricName, data) => {
+const generateRangeTimes = (startTime, endTime, period) => {
+  let timePoint = startTime
+  let dates = []
 
+  while (timePoint < endTime && timePoint < Date.now()) {
+    dates.push(timePoint)
+    timePoint += period * 1000
+  }
+
+  return dates
+}
+
+const formatResponseData = (metricData, { startTime, endTime, period }) => {
+  const originRangeTimes = generateRangeTimes(Number(startTime), Number(endTime), Number(period))
+
+  const indices = metricData.reduce(
+    (indices, datapoint ) => ({
+      ...indices,
+      [ new Date(datapoint.timestamp).getTime() ]: datapoint.value
+    }),
+    {}
+  )
+
+  const datapoints = originRangeTimes.map((time) => ({
+    timestamp: time,
+    value: indices[ time ] || 0
+  }))
+
+  return datapoints
+}
+
+const update = async (projectIdentifier, metricName, data) => {
   if (!projectIdentifier || !metricName) {
     return null
   }
@@ -14,14 +46,14 @@ const update = async (projectIdentifier, metricName, data) => {
     data.map(async ({ timestamp, value }) => {
        const checkExistsData = await elasticsearchService.head(
          `${ DATAPOINT_VERSION }-${ projectIdentifier }-${ metricName }`,
-         `${ metricName }`,
+         metricName,
          timestamp
        )
 
        if (checkExistsData) {
          return await elasticsearchService.replace(
            `${ DATAPOINT_VERSION }-${ projectIdentifier }-${ metricName }`,
-           `${ metricName }`,
+           metricName,
            timestamp,
            { timestamp: new Date(timestamp), value }
          )
@@ -29,7 +61,7 @@ const update = async (projectIdentifier, metricName, data) => {
 
        return await elasticsearchService.create(
          `${ DATAPOINT_VERSION }-${ projectIdentifier }-${ metricName }`,
-         `${ metricName }`,
+         metricName,
          timestamp,
          mapping,
          { timestamp: new Date(timestamp), value }
@@ -38,6 +70,29 @@ const update = async (projectIdentifier, metricName, data) => {
   )
 }
 
+const get = async (projectIdentifier, metricName, data) => {
+  if (!projectIdentifier || !metricName) {
+    return null
+  }
+  const { startTime, endTime, period } = data
+
+  const metricData = await elasticsearchService.list(
+    `${ DATAPOINT_VERSION }-${ projectIdentifier }-${ metricName }`,
+    metricName,
+    {
+      range: {
+        timestamp: {
+          gte: startTime,
+          lte: endTime,
+        }
+      }
+    }
+  )
+
+  return await formatResponseData(metricData, { startTime, endTime, period })
+}
+
 export default {
+  get,
   update
 }
